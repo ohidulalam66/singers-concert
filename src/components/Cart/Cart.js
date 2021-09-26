b3914893a2f7174537053b45eb0e1b60cc90f6bb
ohidulalam <mdnoyonctg15@gmail.com>
2021-09-26 17:39:21 +0600

    Readme file first edit

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -11,21 +11,25 @@ const Cart = props => {
     // total singer quantity select & salary select
     let totalQuantity = 0;
     let totalSalary = 0;
+    let addName = [];
     for(const singer of cart) {
         if(!singer.quantity) {
             singer.quantity = 1;
         }
         totalQuantity = totalQuantity + singer.quantity;
         totalSalary = totalSalary + singer.quantity * singer.salary;
+        addName.push(singer.name);
     }
 
     return (
         <div className= "w-50">
             <div className="card position-fixed cart">
-            <h5 className= "cart-header">{shoppingCartIcon} Concert Presentation </h5>
+            <h5 className= "cart-header">{shoppingCartIcon}Concert Presentation</h5>
                 <div className="card-body">
                     <h5><b>Selected Musician:</b> {totalQuantity}</h5>
-                    <h5><b>Total Salary:</b> {totalSalary}</h5>
+                    <h5><b>Total Salary:</b> ${totalSalary}</h5> 
+                    <h5>{addName}</h5>
+                    <br />
                 </div>
             </div>
         </div>
